a98aa30a7a08b2f919b8528fe66c630687c10a04
Matt E <mersted@gatech.edu>
2014-02-27 20:55:39 -0500

    added proportions for geo data

--- analysis/counts.py
+++ analysis/counts.py
@@ -1,12 +1,15 @@
 from datetime import datetime
 import csv
 import calendar
+import string
 
 interval = 60
 start = '2014-02-02 18:30:00'
 startdate = datetime.strptime(start, "%Y-%m-%d %H:%M:%S")
 millistart = calendar.timegm(startdate.utctimetuple())
 buckets = {}
+wordcount = {}
+unicodeerrors = 0
 
 with open('geo-text-time.tsv', 'rt') as tsvin:
     tsvin = csv.reader(tsvin, delimiter='\t')
@@ -18,13 +21,57 @@ with open('geo-text-time.tsv', 'rt') as tsvin:
             milli = calendar.timegm(date.utctimetuple())
             keyvalue = int((milli - millistart) / 60)
             if keyvalue in buckets:
-                buckets[keyvalue] += row
+                buckets[keyvalue]["tweets"] += row
             else:
-                buckets[keyvalue] = [row]
+                buckets[keyvalue] = {"tweets" : [row], "counts" : {}}
+            
+            tweet = row[0]
+            listofblocks = tweet.split();
+            for block in listofblocks:
+                word = str(block).lower()
+                for char in block:
+                    if char in string.punctuation:
+                        word = word.replace(char, "")
+                if word in wordcount:
+                    wordcount[word] += 1
+                else:
+                    wordcount[word] = 1
+                if word in buckets[keyvalue]["counts"]:
+                    buckets[keyvalue]["counts"][word] += 1   
+                else:
+                    buckets[keyvalue]["counts"][word] = 1
 
 for interval, tweets in buckets.items():
     length = len(tweets)
     thelength = str(length)
     theinterval = str(interval)
-    print(theinterval + " " + thelength)
+    #print(theinterval + " " + thelength)
+
+averages = {}
+
+for word in wordcount:
+    value = wordcount[word] / float(len(buckets.values()))
+    averages[word] = value
     
+over = {}
+
+for interval, tweets in buckets.items():
+    for word in wordcount:
+        if word in buckets[interval]["counts"]:
+            num = buckets[interval]["counts"][word]
+            if num > averages[word]:
+                if word in over:
+                    over[word] += (interval,)
+                if word not in over:
+                    over[word] = (interval,)
+            
+
+print(wordcount["seahawks"])
+print(wordcount["manning"])
+print(averages["seahawks"])
+print(averages["manning"])
+print(over["seahawks"])
+
+
+
+
