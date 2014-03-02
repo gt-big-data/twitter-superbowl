52494b84773071662d028d770d4eb5bd4ebf3e0e
Patrick Violette <violette.patrick718@gmail.com>
2014-03-02 16:55:41 -0500

    started sentiment analysis

--- analysis/random-sample.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# a program to randomly sample a file
-awk 'BEGIN {srand()} !/^%/ {if (rand() < 0.1) print $0}' tweets.json |\
-    jq -c 'select(.text) | {created_at, text}' > 5percent.json
