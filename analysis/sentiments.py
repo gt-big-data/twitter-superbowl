91d7eb6c658ac6decffbe46a6eb16a9fb4618f7e
Matt E <mersted@gatech.edu>
2014-05-06 11:12:59 -0700

    found sentiment per minute

--- analysis/sentiments.py
+++ analysis/sentiments.py
@@ -11,7 +11,7 @@ class MRSentiment(MRJob):
         self.weights = {}
         start = '2014-02-02 18:30:00'
         startdate = datetime.strptime(start, "%Y-%m-%d %H:%M:%S")
-        millistart = calendar.timegm(startdate.utctimetuple())
+        self.millistart = calendar.timegm(startdate.utctimetuple())
         with open("sentiments.txt", "r") as f:
             for line in f:
                 ls = re.split('\s+', line.strip().lower())
@@ -24,11 +24,13 @@ class MRSentiment(MRJob):
         tweet = json.loads(tweet)
         if tweet["text"]:
             timedate = tweet["created_at"].split()
-            date = datetime.strptime(timedate, "%Y-%m-%d %H:%M:%S")
+            time = "2014-02-02 " + timedate[3]
+            date = datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
             milli = calendar.timegm(date.utctimetuple());
-            minute = int((milli - millistart) / 60)
+            minute = int((milli - self.millistart) / 60)
             ls = tweet["text"].lower().split()
             ls = [word for word in ls if not word.startswith('@')]
+            ls = [word for word in ls if not word.startswith('\\')]
             count = 0
             for word in ls:
                 if word in self.weights:
